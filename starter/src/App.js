e8d5610390d988febcebb0cd840ae1ed263e7bd1
moldir <moldirshb@gmail.com>
2023-01-24 10:41:03 -0600

    part1

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* This is where you will code for some of Part 1. */}
+      <Routes>
+        <Route element={<HomeScreen />} />
+        <Route path='newRecipe' element={<NewRecipeScreen />} />
+        <Route path='recipe/:id'element={<DetailScreen />} />
+      </Routes>
       <Footer />
     </div>
   );
